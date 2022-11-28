83c2ba62cc630a61a2ab893b977bce2c08d03f47
leejaneee <91508529+leejaneee@users.noreply.github.com>
2022-11-27 21:09:59 -0500

    puzzle*3+audio+animation trigger

--- Assets/Scripts/Charactermove.cs
+++ Assets/Scripts/Charactermove.cs
@@ -8,6 +8,8 @@ public class Charactermove : MonoBehaviour
     public float smoothing;
     public Vector3 PlayerMovementInput;
     public Transform m_transform;
+    public float rotationspeed;
+
 
     [SerializeField] private Rigidbody PlayerBody;
     [SerializeField] private Transform PlayerCamera;
@@ -22,6 +24,8 @@ public class Charactermove : MonoBehaviour
     }
     void Update()
     {
+
+       
         PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
         MovePlayer();
 
@@ -31,11 +35,16 @@ public class Charactermove : MonoBehaviour
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+
+            m_transform.Rotate(0.0f, rotationspeed*-1, 0.0f, Space.Self);
             m_transform.Translate(Vector3.left * Time.deltaTime * Speed);
+
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
+            m_transform.Rotate(0.0f, rotationspeed, 0.0f, Space.Self);
             m_transform.Translate(Vector3.right * Time.deltaTime * Speed);
+
         }
 
         if (Input.GetKey(KeyCode.UpArrow))
