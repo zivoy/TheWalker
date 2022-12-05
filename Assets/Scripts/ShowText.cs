d7a2ec8709f9c75b277e2f1ebf3c75800ed63389
zivoy <ziv132@gmail.com>
2022-12-05 18:23:31 -0500

    formatted file

--- Assets/Scripts/ShowText.cs
+++ Assets/Scripts/ShowText.cs
@@ -1,26 +1,25 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class ShowText : MonoBehaviour{
-
+public class ShowText : MonoBehaviour
+{
     public GameObject Object;
+
     void Start()
     {
         Object.SetActive(false);
     }
-    
+
 
     void OnTriggerEnter()
     {
         Object.SetActive(true);
-        StartCoroutine("WaitForSec");
+        StartCoroutine(WaitForSec());
     }
+
     IEnumerator WaitForSec()
     {
         yield return new WaitForSeconds(5);
         Destroy(Object);
-            }
-    
+    }
 }
