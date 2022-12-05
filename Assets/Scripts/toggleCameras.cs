8c7aaabc0f657b1f915dd6c61308e562f1cfaeca
zivoy <ziv132@gmail.com>
2022-12-05 03:08:13 -0500

    added toggleable 3rd person mode

---
+++ Assets/Scripts/toggleCameras.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class toggleCameras : MonoBehaviour
+{
+    public Camera cameraObj; 
+    public MonoBehaviour firstPersonScript; 
+    public MonoBehaviour thirdPersonScript;
+    public KeyCode toggleKey = KeyCode.F5;
+    public string playerLayer = "Player";
+    
+    private bool _firstPerson = true;
+    private int maskBit;
+
+    void updateScripts()
+    {
+        firstPersonScript.enabled = _firstPerson;
+        thirdPersonScript.enabled = !_firstPerson;
+
+        if (_firstPerson) cameraObj.cullingMask &= ~maskBit;
+        else cameraObj.cullingMask |= maskBit;
+    }    
+    
+    void Start()
+    {
+        maskBit = 1 << LayerMask.NameToLayer(playerLayer);
+        updateScripts();
+    }
+
+    void Update()
+    {
+        if (!Input.GetKeyDown(toggleKey)) return;
+        
+        _firstPerson = !_firstPerson;
+        updateScripts();
+    }
+}
