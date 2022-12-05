1c1c39441b8bcf8ff32c14605def79bc06988a65
tizza555 <tyler.jorgemoore@gmail.com>
2022-12-04 22:37:39 -0500

    Object Interaction v1
    - Added basic object interaction
        - Ability to press 'E' on applicable objects, will destroy them at current stage in
           development
    
    - Changed FirstPersonTEST scene to Object Interaction Scene
    - Removed some redundant variables from PlayerController.cs
    - Added 'Interaction' and 'Ship Part' tags
    - Removed previous Interaction scripts that no longer worked

---
+++ Assets/Scripts/Object Interaction/ShipPart.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipPart : MonoBehaviour {
+
+    public enum Part {part1, part2, part3};
+    public ShipPart whatPartAmI;
+}
